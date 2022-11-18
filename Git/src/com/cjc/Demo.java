d55418cfaff7790442e18fefb0789bcc87cca645
adhya <adhya@Adhyayan05>
2022-11-18 13:01:32 +0530

    dfsrdfhjkm

--- Git/src/com/cjc/Demo.java
+++ Git/src/com/cjc/Demo.java
@@ -1,11 +1,17 @@
 package com.cjc;
 
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello");
-		public void m1()
-		{
-			System.out.println("m1 method");
-		}
+		
 	}
+
+
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+
 }
+

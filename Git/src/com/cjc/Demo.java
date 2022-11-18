b564b8d5a3ae233a289187c669d7abc6d36c941a
patilkiran24 <118417706+patilkiran24@users.noreply.github.com>
2022-11-18 12:57:19 +0530

    Update Demo.java

--- Git/src/com/cjc/Demo.java
+++ Git/src/com/cjc/Demo.java
@@ -3,5 +3,9 @@ package com.cjc;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		public void m1()
+		{
+			System.out.println("m1 method");
+		}
 	}
 }
